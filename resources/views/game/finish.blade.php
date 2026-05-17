25a0118ab8581d3c2ae14d193e3a357e55419705
Rava <ravanello2626@gmail.com>
2026-05-17 19:28:54 +0700

    fix: add trusted proxies for railway https

--- resources/views/game/finish.blade.php
+++ resources/views/game/finish.blade.php
@@ -2,22 +2,33 @@
 <html>
 <head>
     <title>Misi Selesai</title>
+
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}?v={{ time() }}">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
 <body class="game-bg">
 
-<div class="finish-box">
-    <h1>MISI SELESAI!</h1>
-    <h2>Materi: {{ $materi->nama_materi }}</h2>
-    <hr>
-    <h3>SKOR AKHIR ANDA:</h3>
-    <h1 style="font-size: 80px; color: yellow; text-shadow: 5px 5px black;">{{ $score }}</h1>
-    
-    <div class="menu-btn">
-        <a href="/" class="btn red">KEMBALI KE BERANDA</a>
+    <div class="finish-box">
+
+        <h1>MISI SELESAI!</h1>
+
+        <h2>Materi: {{ $materi->nama_materi }}</h2>
+
+        <hr>
+
+        <h3>
+            Selamat! Anda telah menyelesaikan seluruh POS pada materi ini.
+        </h3>
+
+        <div class="menu-btn">
+            <a href="/" class="btn red">
+                KEMBALI KE BERANDA
+            </a>
+        </div>
+
     </div>
-</div>
 
 </body>
 </html>
