67d3cfef7f1244bb37acdcbe52afc938f94a2b82
Rava <ravanello2626@gmail.com>
2026-05-17 21:16:24 +0700

    fix: add trusted proxies for railway https

--- resources/views/game/home.blade.php
+++ resources/views/game/home.blade.php
@@ -6,7 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body class="game-bg">
+    <div id="loading-screen">
+    <div class="loader-box">
+        <h1>LOADING...</h1>
+        <div class="loader-bar">
+            <div class="loader-fill"></div>
+        </div>
+        <p>Menyiapkan Petualangan...</p>
+    </div>
 </div>
+
+<div id="main-content" style="display:none;">
+</div> 
 
 <div class="board-home">
     <h1>GAME MEDIA PEMBELAJARAN</h1>
@@ -25,6 +36,20 @@
 
 <div class="mario-run"></div>
 
+</div>
+<script>
+window.addEventListener("load", function(){
+
+    setTimeout(function(){
+
+        document.getElementById("loading-screen").style.display = "none";
+        document.getElementById("main-content").style.display = "block";
+
+    }, 3000); // loading 3 detik
+
+});
+</script>
 <script src="{{ asset('assets/js/game.js') }}"></script>
+
 </body>
 </html>
